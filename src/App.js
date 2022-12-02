48e1317f57cd611e278f74127e4c0170c1895fa9
SaadiaElf <saadiaelfekak@gmail.com>
2022-12-02 11:28:37 +0000

    Added react router

--- src/App.js
+++ src/App.js
@@ -1,76 +1,14 @@
-import React, { useEffect, useState } from "react";
-import Header from "./components/Header";
-import Container from "./components/Container";
-import Card from "./components/Card";
-import Search from "./components/Search";
-import Select from "./components/Select";
-import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
 
 function App() {
-  const [allCountries, setAllCountries] = useState([]);
-  const [countries, setCountries] = useState([]);
-  const [regionCountries, setRegionCountries] = useState([]);
-
-  useEffect(() => {
-    fetch("https://restcountries.com/v3.1/all")
-      .then((response) => response.json())
-      .then((data) => {
-        setCountries(data);
-        setAllCountries(data);
-        setRegionCountries(data);
-      });
-  }, []);
-
-  function search(e) {
-    if (e.target.value) {
-      const filtered = regionCountries.filter((country) => {
-        return country.name.common
-          .toLowerCase()
-          .includes(e.target.value.toLowerCase());
-      });
-      setCountries(filtered);
-    } else {
-      setCountries(regionCountries);
-    }
-  }
-
-  function filterByRegion(e) {
-    if (e.target.value === "-1") {
-      setCountries(allCountries);
-    } else {
-      let filteredCountries = allCountries.filter((country) => {
-        return country.region
-          .toLowerCase()
-          .includes(e.target.value.toLowerCase());
-      });
-      setCountries(filteredCountries);
-      setRegionCountries(filteredCountries);
-    }
-  }
-
   return (
-    <div className="App">
-      <Header />
-      <Container className="container--secondary">
-        <Search onchange={search} />
-        <Select onchange={filterByRegion} />
-      </Container>
-      <Container className="container--primary">
-        {countries.map((country, index) => {
-          return (
-            <Card
-              key={index}
-              imageSrc={country.flags.png}
-              imageAlt={country.name.common}
-              title={country.name.common}
-              population={country.population}
-              region={country.region}
-              capital={country.capital}
-            />
-          );
-        })}
-      </Container>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
