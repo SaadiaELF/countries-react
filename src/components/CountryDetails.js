416bbfbe3da40efe21a3b9c6cf1bc64cf57f3652
SaadiaElf <saadiaelfekak@gmail.com>
2022-12-02 16:11:45 +0000

    Getting data

--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -4,28 +4,28 @@ function CountryDetails(props) {
   const [country, setCountry] = useState({});
 
   useEffect(() => {
-    // fetch(`https://restcountries.com/v3.1/name/${props.countryName}`)
-    //   .then((response) => response.json())
-    //   .then((data) => {
-    //     setCountry(data);
-    //   });
-    const fetchData = async () => {
-      const data = await fetch(
-        `https://restcountries.com/v3.1/name/${props.countryName}`
-      );
-      const json = await data.json();
-      setCountry(json[0]);
-    };
-    fetchData()
-      // make sure to catch any error
-      .catch(console.error);
+    fetch(`https://restcountries.com/v3.1/name/${props.countryName}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setCountry(data[0]);
+      });
+    // const fetchData = async () => {
+    //   const data = await fetch(
+    //     `https://restcountries.com/v3.1/name/${props.countryName}`
+    //   );
+    //   const json = await data.json();
+    //   setCountry(json[0]);
+    // };
+    // fetchData()
+    //   // make sure to catch any error
+    //   .catch(console.error);
   }, [props.countryName]);
 
   console.log(country);
 
   return (
     <div className="container container--country">
-      {country ? (
+      {Object.keys(country).length !== 0 ? (
         <div className="card--country">
           <img
             className="card__img--country"
@@ -36,10 +36,10 @@ function CountryDetails(props) {
             <h2 className="card__title--country">{country.name.common}</h2>
             <div className="card__lists--country">
               <ul className="card__list--country">
-                {/* <li>
+                <li>
                 <span>Native name: </span>
-                {props.nativeName}
-              </li> */}
+                {Object.values(country.name.nativeName)[0].common}
+              </li>
                 <li>
                   <span>Population: </span>
                   {country.population}
@@ -57,25 +57,27 @@ function CountryDetails(props) {
                   {country.capital}
                 </li>
               </ul>
-              {/* <ul className="card__list--country">
-              <li>
-                <span>Top Level Domain: </span>
-                {props.domain}
-              </li>
-              <li>
-                <span>Currencies: </span>
-                {props.currencies}
-              </li>
-              <li>
-                <span>Languages: </span>
-                {props.languages}
-              </li>
-            </ul> */}
+              <ul className="card__list--country">
+                <li>
+                  <span>Top Level Domain: </span>
+                  {country.tld}
+                </li>
+                <li>
+                  <span>Currencies: </span>
+                  {Object.keys(country.currencies)[0]}
+                </li>
+                <li>
+                  <span>Languages: </span>
+                  {Object.values(country.languages).map((language) => {
+                    return <span>{language}</span>;
+                  })}
+                </li>
+              </ul>
             </div>
           </div>
         </div>
       ) : (
-        ""
+        "Loading"
       )}
     </div>
   );
